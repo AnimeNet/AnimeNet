f45dc77a527318d23efc0ac34bb9f62dc770df3e
AnimeNet <info.animenet@gmail.com>
2021-04-01 16:47:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="H@ck">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./res/style/style.css">
+
+    <script src="https://kit.fontawesome.com/926a224c23.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+
+    <title>AnimeNet | Home</title>
+</head>
+<body>
+    <header class="header">
+        
+        <div class="logo_container">
+            <img src="./res/image/logo.svg" class="logo_image" width="200px">
+        </div>
+
+        <div class="instagram_container">
+            <a href="https://www.instagram.com/animenet_ita/" target="_blank">
+                <h2>AnimeNet</h2>
+                <p href="">Seguici su instagram</p>
+                <i class="fab fa-instagram"></i>
+            </a>
+        </div>
+
+        <div class="discord_container">
+            <a href="https://discord.gg/vuwmybAw" target="_blank">
+                <h2>Discord</h2>
+                <p href="#">Unisciti al server discord</p>
+                <i class="fab fa-discord"></i>
+            </a>
+        </div>
+
+        <div class="logo_curve">
+        </div>
+
+    </header>
+
+    <div class="button_container">
+        <a href="https://www.instagram.com/animenet_ita/" target="_blank">Instagram</a>
+        <a href="https://www.amazon.it/hz/wishlist/ls/1VWEK1FBB5MKV?ref_=wl_share" target="_blank">Wishlist</a>
+        <a href="#">Cooming Soon</a>
+        <a href="https://t.me/animenet_ita">Telegram</a>
+        <a href="https://discord.gg/vuwmybAw">Discord</a>
+    </div>
+    
+    <footer class="footer">
+            <p>Copyright © AnimeNet All right Reserved</p>
+    </footer>
+
+</body>
+</html>
